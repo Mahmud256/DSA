29d900cb4a33b5766b0ab5259c8e9c3b48dee3be
Mahmudul Hasan Sarkar <mahmud474849@gmail.com>
2026-01-07 01:43:21 +0600

    add new file

---
+++ Greedy/CodeForces-946A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    long long ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        long long x;
+        cin >> x;
+        ans += abs(x);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
