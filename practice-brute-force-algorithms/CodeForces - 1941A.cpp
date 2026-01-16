2149d0dce2f8a8b4fd58a875232f3c400039459b
Mahmudul Hasan Sarkar <mahmud474849@gmail.com>
2026-01-16 23:31:32 +0600

    add file

---
+++ practice-brute-force-algorithms/CodeForces - 1941A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n,m,k;
+        cin>>n>>m>>k;
+
+        int left[n + 1], right[m + 1];
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>left[i];
+        }
+
+        for(int i=0; i<m; i++)
+        {
+            cin>>right[i];
+        }
+
+        int count = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(left[i] + right[j] <= k)
+                {
+                    count++;
+                }
+            }
+        }
+
+        cout<<count<<endl;
+    }
+
+    return 0;
+}
